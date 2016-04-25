1a12757530c9b8fb619fb186445f84c2da89b9bf
ZulkiflXulfi <zulkiflxulfi@gmail.com>
2016-04-25 07:42:15 +0500

    say hello to github

---
+++ ScM/src/scm/Mirza.java
@@ -0,0 +1,19 @@
+package scm;
+
+import java.util.Scanner;
+
+public class Mirza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int a=55;
+
+Scanner sc=new Scanner(System.in);
+
+a=sc.nextInt();
+System.out.println(a);
+
+	}
+
+}
